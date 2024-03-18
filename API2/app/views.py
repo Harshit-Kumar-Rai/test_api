0d0b97edc9ad4e4c0b33d6e40df012928c7bcb2d
Harshit Kumar Rai <106249416+Harshit-Kumar-Rai@users.noreply.github.com>
2024-03-18 17:16:54 +0530

    Update views.py

--- API2/app/views.py
+++ API2/app/views.py
@@ -2,17 +2,23 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
-from.serializer import CompanyRegisterSeralizer, SendDataSeralizer
+from.serializer import CompanyRegisterSeralizer, SendDataSeralizer, UserSeralizer
 from .models import Company
 
 @api_view(['GET', 'POST'])
 
-def validate_user(request, username, password):
-  if request.method == 'GET':
-    try:
-      data = Company.objects.get(username=username, password = password)
-    except:
-      return Response({'error':'Invalid user data'},status=status.HTTP_400_BAD_REQUEST)
+def validate_user(request):
+  if request.method == 'POST':
+    serializer = UserSeralizer(data=request.data)
+    if serializer.is_valid():
+      validated_data = serializer.validated_data
+      username = validated_data.get('username')
+      password = validated_data.get('password')
+      
+      try:
+        data = Company.objects.get(username = username, password = password)
+      except:
+        return Response({'error':'Invalid user data'},status=status.HTTP_400_BAD_REQUEST)
+      userdata = SendDataSeralizer(data)
+      return Response (userdata.data)
     
-    serializer = SendDataSeralizer(data)
-    return Response(serializer.data)
